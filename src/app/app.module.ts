978a1ce69a5725daedd8f69f86e1dca5243cfcf1
felicfingki <felicfingki@yahoo.com>
2021-03-02 18:31:52 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-import { halaman1Component } from './halaman1/halaman1.component';
-import { halaman2Component } from './halaman2/halaman2.component';
-import { halaman3Component } from './halaman3/halaman3.component';
+import { halaman1Component } from '../halaman1/halaman1.component';
+import { halaman2Component } from '../halaman2/halaman2.component';
+import { halaman3Component } from '../halaman3/halaman3.component';
+
+import {Routes} from '@angular/router';
+
+const ROUTES : Routes = [
+  (path : 'halaman1', component : halaman1Component),
+  (path : 'halaman2', component : halaman2Component),
+  (path : 'halaman3', component : halaman3Component)
+]
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
