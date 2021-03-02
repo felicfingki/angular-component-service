73e3dd4e27ad4bb92eb3060a4b7ec562d169e17e
felicfingki <felicfingki@yahoo.com>
2021-03-02 18:25:54 +0700

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { halaman1Component } from './halaman1/halaman1.component';
+import { halaman2Component } from './halaman2/halaman2.component';
+import { halaman3Component } from './halaman3/halaman3.component';
+
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, halaman1Component, halaman2Component, halaman3Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
